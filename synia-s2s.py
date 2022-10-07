052970c796baa7dbc1a88b68a79a08a00fab9167
Sebastian <44552816+bazeroni@users.noreply.github.com>
2022-10-07 11:04:48 -0400

    implement whisper stt

--- synia-s2s.py
+++ synia-s2s.py
@@ -10,38 +10,47 @@ import numpy as np
 import pickle
 import random
 import json
-from gtts import gTTS
 import os
 import time
-import boto3
-from boto3 import Session
-from botocore.exceptions import BotoCoreError, ClientError
 import openai
 from dotenv import load_dotenv
 import azure.cognitiveservices.speech as speechsdk
 import xml.etree.ElementTree as ET
 import xmltodict
+import tempfile
+import whisper
+import speech_recognition as sr
+from pydub import AudioSegment
+import io
 
 # loads env variables file
 load_dotenv()
 
 ### AUTH KEYS ###
+
 AZURE_SPEECH_KEY = os.getenv("AZURE") #AZURE
 OAI_API_KEY = os.getenv("YOUR_API_KEY") #OPENAI
 openai.api_key=OAI_API_KEY #OPEN AI INIT
-AWS_ID_KEY = os.getenv("AWS_ID_KEY") #AWS ID
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY") #AWS KEY
-WIT_AI_KEY = os.getenv("WIT_AI_KEY") #WIT
 
 ### AZURE ###
-# configs tts voice
+
+# configs tts
 speech_config = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_KEY, region="eastus")
-#speech_config.speech_recognition_language="en-US"
-speech_config.speech_recognition_language="es-US"
+
+# configs stt lang
+speech_config.speech_recognition_language="en-US"
 #speech_config.speech_recognition_language="fr-CA"
-#speech_config.speech_synthesis_voice_name='en-US-AriaNeural'
-speech_config.speech_synthesis_voice_name='es-MX-CarlotaNeural'
+#speech_config.speech_recognition_language="es-US"
+#speech_config.speech_recognition_language="fa-IR"
+
+# configs tts voice
+speech_config.speech_synthesis_voice_name='en-US-AriaNeural'
+#speech_config.speech_synthesis_voice_name='en-US-AIGenerate1Neural'
 #speech_config.speech_synthesis_voice_name = 'zh-CN-XiaomoNeural'
+#speech_config.speech_synthesis_voice_name='es-MX-CarlotaNeural'
+#speech_config.speech_synthesis_voice_name = 'fa-IR-FaridNeural'
+
+# sets voice
 voice = speech_config.speech_synthesis_voice_name
 
 # microphone device stt 
@@ -53,13 +62,15 @@ tts_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=stt_config)
 # inits tts
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=tts_config)
+# sets tts sample rate
 speech_config.set_speech_synthesis_output_format(speechsdk.SpeechSynthesisOutputFormat.Riff44100Hz16BitMonoPcm)
 
-# sets up identifiers for conversation
-user = "Sebastian"
-bot = "Carlota"
 
-### IMPORTANT SETUP VARIABLES ###
+# sets up identifiers for conversation
+user = "Seb"
+bot = "Jenny"
+
+### SETUP VARIABLES ###
 # concats message history for re-insertion with every prompt
 context = ""
 # last response from GPT
@@ -99,8 +110,8 @@ def tone_gpt3(zice):
 def chat_gpt3(zice):
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt= bot+" is speaking Spanish wih "+user+" Spanish."+context+"\n"+user+": "+zice+"\n"+bot+":",
-        #prompt= "This is a conversation between "+bot+" and "+user+" in a Discord voice chat. "+bot+" leads the conversation by asking fun and interesting questions, and switches up the topic when things get boring. "+bot+" is witty, sarcastic, and brutally honest."+context+"\n"+user+": "+zice+"\n"+bot+" ["+style+"]:",
+        #prompt= bot+" is helping "+user+" speak Spanish. "+context+"\n"+user+": "+zice+"\n"+bot+":",
+        prompt= "This is a conversation between "+bot+" and "+user+" in a Discord voice chat. "+bot+" leads the conversation by asking fun and interesting questions, and switches up the topic when things get boring. "+bot+" is witty, sarcastic, and brutally honest."+context+"\n"+user+": "+zice+"\n"+bot+" ["+style+"]:",
         temperature=1.0,
         max_tokens=256,
         top_p=1.0,
@@ -119,6 +130,41 @@ def tts(ssml):
     global speech_synthesis_result
     speech_synthesis_result = speech_synthesizer.speak_ssml_async(ssml).get()
 
+temp_dir = tempfile.mkdtemp()
+save_path = os.path.join(temp_dir, "temp.wav")
+
+def stt(model="base", english=False, verbose=False, energy=300, pause=0.5, dynamic=True):
+    
+    audio_model = whisper.load_model(model)
+    
+    r = sr.Recognizer()
+    r.energy_threshold = energy
+    r.pause_threshold = pause
+    r.dynamic_energy_threshold = dynamic
+
+    with sr.Microphone(sample_rate=16000) as source:
+            
+        # prints status
+        print("|||||||||| LISTENING ||||||||||")
+        
+        audio = r.listen(source)
+        data = io.BytesIO(audio.get_wav_data())
+        audio_clip = AudioSegment.from_file(data)
+        audio_clip.export(save_path, format="wav")
+
+        if english:
+            result = audio_model.transcribe(save_path,language='english')
+        else:
+            result = audio_model.transcribe(save_path)
+
+        if not verbose:
+            predicted_text = result["text"]
+            print("You said: " + predicted_text)
+        else:
+            print(result)
+    
+    return predicted_text
+
 while (True):
 
     try:
@@ -133,6 +179,7 @@ while (True):
             
             # parses and formats user input
             prompt = user+": "+inp
+
             # global counter and helper variables
             global silenceCount
             global context
@@ -142,27 +189,35 @@ while (True):
 
             # processes non-silent interaction
             if inp != "":
+                
+                # prints status
                 print("NON-SILENCE")
                 print(prompt)
+
                 # gets style GPT would like to respond with
-                #style = ((tone_gpt3(inp)).choices[0].text).split("]")[0]
+                style = ((tone_gpt3(inp)).choices[0].text).split("]")[0]
                 #print(style)
+
                 # gets GPT text message response completion
                 raspuns = (chat_gpt3(inp)).choices[0].text
+
                 # formats raspuns
-                #raspunsF = bot+" ["+style+"]: " + raspuns
-                raspunsF = bot+": "+raspuns
+                raspunsF = bot+" ["+style+"]: " + raspuns
+                #raspunsF = bot+": "+raspuns
+
+                # prints raspuns
                 print(raspunsF)
                 #print(raspuns)
+
                 # concats message to memory/history
                 context += "\n"+prompt+"\n"+raspunsF
+
                 # SSML for TTS with response and style
                 xmlStringReg = '''<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US">
-    <voice name="'''+voice+'''">
-        '''+ raspuns +'''
-    </voice>
-</speak>'''
-                
+                <voice name="'''+voice+'''">
+                '''+ raspuns +'''
+                </voice>
+                </speak>'''
                 xmlString = '''<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
                 xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
                 <voice name="'''+voice+'''">
@@ -173,35 +228,52 @@ while (True):
                 </prosody>
                 </voice>
                 </speak>'''
+
                 # synthesizes TTS with input SSML
                 tts(xmlStringReg)
+
                 # resets silence count to 0
                 silenceCount = 0
+
                 # marks interaction
                 done = True
+
             # processes silent interaction
             else:
+
                 # checks if user has been silent for certain amount of time
                 if silenceCount == 2:
+                    
+                    # imitates silent input
                     prompt = user+": ..."
+
+                    # prints status
                     print("SILENCE PROMPT")
                     print(prompt)
+
                     # gets style GPT would like to respond with
-                    #style = ((tone_gpt3("...")).choices[0].text).split("]")[0]
+                    style = ((tone_gpt3("...")).choices[0].text).split("]")[0]
                     #print(style)
+
                     # gets GPT text message response completion
                     raspuns = (chat_gpt3("...")).choices[0].text
+
                     # formats raspuns
-                    raspunsF = bot+": "+raspuns
+                    raspunsF = bot+" ["+style+"]: " + raspuns
+                    #raspunsF = bot+": "+raspuns
+
+                    # prints raspuns
                     print(raspunsF)
+
                     # concats message to memory/history
                     context += "\n"+prompt+"\n"+raspunsF
+
                     # SSML for TTS with response and style
                     xmlStringReg = '''<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US">
-    <voice name="'''+voice+'''">
-        '''+ raspuns +'''
-    </voice>
-</speak>'''
+                    <voice name="'''+voice+'''">
+                    '''+ raspuns +'''
+                    </voice>
+                    </speak>'''
                     xmlString = '''<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
                     xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">
                     <voice name="'''+voice+'''">
@@ -212,21 +284,37 @@ while (True):
                     </prosody>
                     </voice>
                     </speak>'''
+
                     # synthesizes TTS with input SSML
                     tts(xmlStringReg)
+
                     # resets silence count to 0
                     silenceCount = 0
+
                     # marks interaction
                     done = True
+
                 # increases silence count
                 print("SILENCE")
                 silenceCount += 1
         
         # listens for speech
         while not done:
-            print("|||||||||| LISTENING ||||||||||")
-            speech_recognition_result = speech_recognizer.recognize_once_async().get()
-            speech_recognizer.recognized.connect(textSpeech(speech_recognition_result.text))
             
+            print("|||||||||| LISTENING ||||||||||")
+
+            # gets azure stt
+            speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+            # gets tts from azure stt
+            speech_recognizer.recognized.connect(textSpeech(speech_recognition_result.text))
+
+            # gets whisper stt
+            #speech_recognition_result = stt()
+
+            # gets tts from whisper stt
+            #textSpeech(speech_recognition_result)
+
+    # excepts if not understood
     except Exception as e:
         print("Could not understand", e)
